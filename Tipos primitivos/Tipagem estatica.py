253b88d5f8b3628c3b59bd918343f40042d3384f
rduarte0 <stosruan13@gmail.com>
2024-08-16 18:15:36 +0000

    atividade

---
+++ Tipos primitivos/Tipagem estatica.py
@@ -0,0 +1,16 @@
+import os
+
+os.system("cls || clear") #limpar o terminal
+
+# Tipagens estaticas>
+nome : str # cadeia
+idade : int # indeiro
+altura : float # real
+
+nome = "carlos"
+idade = 20
+altura = 1.65
+
+print(f"nome: {nome}")
+print(f"idade: {idade}")
+print(f"altura: {altura}") 
